194aa8ff757056f545b3fa29a01f7bc2deb13551
bohecmian <bohecmian@gmail.com>
2024-10-16 20:59:44 -0400

    final

--- Project5/Assets/Script/Chest.cs
+++ Project5/Assets/Script/Chest.cs
@@ -1,6 +1,7 @@
 using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,12 +9,16 @@ public class Chest : MonoBehaviour
 {
     public GameObject chest;
     public GameObject UI;
+
+    public TextMeshProUGUI TreasureText;
+    float T = 8;
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             UI.SetActive(true);
             if (Input.GetKey(KeyCode.E)){
+                T--;
                 Destroy(chest);
                 UI.SetActive(false);
             }
